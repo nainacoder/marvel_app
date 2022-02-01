f04e451929a40cf2c2f2005916058eb9e73f09db
Sunaina Sahoo <nainacoder@gmail.com>
2022-02-01 17:05:05 +0530

    hashkey error solved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react"
 import HomePage from "./pages/HomePage";
-import {BrowserRouter as Router,  Route} from "react-router-dom"
+
 function App() {
   return (
-    <Router>
-      {/* <Switch> */}
-        {/* <Route exact path="/"> */}
-      <HomePage/>
-      {/* </Route> */}
-      {/* </Switch> */}
-    </Router>
+   <div>
+     <HomePage/>
+   </div>
   );
 }
 
